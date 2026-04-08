23b7e74a6e23fe209ba9f38056a25fff9717f611
Noe <noelia2000vane@gmail.com>
2026-04-08 13:04:20 -0400

    FEAT #10: fix observation

--- src/modules/services/servicesRepository.ts
+++ src/modules/services/servicesRepository.ts
@@ -2,8 +2,18 @@ import { db } from "../../db/knex";
 
 export const getServices = async () => {
   const rows = await db("services")
-    .select("id", "name", "description", "date", "time")
+    .select(
+      "id",
+      "business_id",
+      "name",
+      "description",
+      "price",
+      "schedule",
+      "is_active",
+      "custom_fields",
+      "created_at"
+    )
     .orderBy("id");
 
-  return { rows };
+ return rows;
 };
