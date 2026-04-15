5a49d3379cb043eebf60286abbabc39c9f127b8a
Noe <noelia2000vane@gmail.com>
2026-04-15 16:25:51 -0400

    FEAT #10: add test

---
+++ src/db/migration/V6__Add_default_admin_user.sql
@@ -0,0 +1,11 @@
+INSERT INTO users 
+(name, email, password, user_type, address, phone_number)
+VALUES 
+(
+  'Admin',
+  'test@email.com',
+  '$2b$10$hkqnLbUOnzOn9nq2hOgHo.fFQiNobzs.rcWyo4zBqyKkyTr.W/Nx.',
+  'empresa',
+  'N/A',
+  '00000000'
+);
