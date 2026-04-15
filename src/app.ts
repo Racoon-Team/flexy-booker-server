b63d9c42e525bd6e7a0c15193a639adeebdafe7b
Noe <noelia2000vane@gmail.com>
2026-04-15 11:27:42 -0400

    FEAT #20:database verification running

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+
+
 import todoRoutes from "./modules/todo/todo.routes";
 import userRoutes from "./modules/users/usersRoutes";
 import authRoutes from "./modules/auth/authRoutes";
 import servicesRoutes from "./modules/services/servicesRoutes";
+import { dbReady } from "./server";
+
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use((req: Request, res: Response, next) => {
+  if (!dbReady) {
+    return res.status(500).json({
+      message: "Database not ready",
+    });
+  }
+  next();
+});
 
 app.use("/api", todoRoutes);
 app.use("/api/users", userRoutes);
@@ -19,4 +32,3 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 export default app;
-
